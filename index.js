507eb59eca9b307c8b3d24bedfe4319ed56f1a55
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 17:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,4 +25,21 @@ class Passenger {
     }
 }
 let tripId = 0;
-
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = tripId++;
+    store.trips.push(this);
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
