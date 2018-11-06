41dfe8dbab075f8a2962fbc6c4bbaa6832554bdd
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 17:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Driver {
         }
         passengers() {
             return this.trips.map(trip => {
-              return trip.passenger();
+              return trip.passenger();}
             });
 
 
